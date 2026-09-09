cffb14151f16c8352d1ab637cccb60df3b1a2333
Andreyh117 <rodriguesandreyh@gmail.com>
2026-09-09 20:26:02 -0300

    Completa implementacao da aula 02

--- aula-02-hadoop/src/mapreduce_wordcount.py
+++ aula-02-hadoop/src/mapreduce_wordcount.py
@@ -56,7 +56,9 @@ class MRWordFrequencyCount(MRJob):
             emitido: ("gato", 1), ("correu", 1), ("atrás", 1), ("rato", 1)
             (observe que "o" e "do" são stopwords e foram descartadas)
         """
-        raise NotImplementedError("TODO 1: implemente o mapper")
+        for word in WORD_RE.findall(line.lower()):
+            if word not in STOPWORDS:
+                yield word, 1
 
     def combiner(self, word, counts):
         """
@@ -79,7 +81,7 @@ class MRWordFrequencyCount(MRJob):
         Some todos os valores em `counts` (que é um iterável de números)
         e emita o par (word, total) usando `yield`.
         """
-        raise NotImplementedError("TODO 2: implemente o reducer")
+        yield word, sum(counts)
 
 
 if __name__ == "__main__":
